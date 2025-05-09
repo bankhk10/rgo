8eb9f2b0cf2714c77a28caccfec6ead6e9957dc0
Atthapol <69508240+bankhk10@users.noreply.github.com>
2025-05-09 11:27:08 +0700

    add

---
+++ routes/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log; // Import the Log facade
+
+
+Route::prefix('admin')->name('admin.')->group(function(){
+
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+                    ->middleware('guest')
+                    ->name('login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+                    ->middleware('guest');
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+                    ->middleware('auth')
+                    ->name('logout');
+});
