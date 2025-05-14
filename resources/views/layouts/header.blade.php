17a8149e9cb94c81cf05fd5a39213c1c9c160055
Atthapol <69508240+bankhk10@users.noreply.github.com>
2025-05-14 14:15:04 +0700

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<header class="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-indigo-600">
+<header class="flex justify-between items-center py-4 px-6 bg-white  border-b-4 border-indigo-600 sticky top-0 z-50">
     <div class="flex items-center">
         <button @click="sidebarOpen = true" class="text-gray-500 focus:outline-none lg:hidden">
             <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -7,7 +7,7 @@
             </svg>
         </button>
 
-        <div class="relative mx-4 lg:mx-0">
+        {{-- <div class="relative mx-4 lg:mx-0">
             <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
                 <svg class="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
                     <path
@@ -18,17 +18,15 @@
             </span>
 
             <input class="form-input w-32 sm:w-64 rounded-md pl-10 pr-4 focus:border-indigo-600" type="text"
-                placeholder="Search">
-        </div>
+                   placeholder="Search">
+        </div> --}}
     </div>
 
     <div class="flex items-center">
         <div x-data="{ dropdownOpen: false }" class="relative">
             <button @click="dropdownOpen = ! dropdownOpen"
                 class="relative block h-8 w-8 rounded-full overflow-hidden shadow focus:outline-none">
-                <img class="h-full w-full object-cover"
-                    src="/images/{{ auth()->user()->profile }}"
-                    alt="Your avatar">
+                <img class="h-full w-full object-cover" src="/images/{{ auth()->user()->profile }}" alt="Your avatar">
             </button>
 
             <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"
@@ -41,12 +39,14 @@
                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Profile</a>
 
                 <form method="POST" action="{{ route('admin.logout') }}">
-                @csrf
-                    <a href="{{ route('admin.logout') }}" onclick="event.preventDefault();
+                    @csrf
+                    <a href="{{ route('admin.logout') }}"
+                        onclick="event.preventDefault();
                                                 this.closest('form').submit();"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Logout</a>
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Logout</a>
                 </form>
             </div>
         </div>
     </div>
 </header>
+
