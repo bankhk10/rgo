9ea915ec00a8c6d4d7ff3b8b596cd23777c8790c
Atthapol <69508240+bankhk10@users.noreply.github.com>
2025-07-10 09:21:45 +0700

    add

--- resources/views/setting/user/index.blade.php
+++ resources/views/setting/user/index.blade.php
@@ -24,8 +24,8 @@
                         <table class="min-w-full bg-white">
                             <thead>
                                 <tr class="bg-indigo-600 text-white text-left">
-                                    <th class="py-4 px-6 rounded-tl-2xl">ลำดับ</th>
-                                    <th class="py-3 px-6">ชื่อผู้ใช้งาน</th>
+                                    <th class="py-4 px-6 rounded-tl-2xl">ชื่อผู้ใช้งาน</th>
+                                    {{-- <th class="py-3 px-6">ชื่อผู้ใช้งาน</th> --}}
                                     <th class="py-3 px-6">อีเมล์เข้าสู่ระบบ</th>
                                     <th class="py-3 px-6">สิทธิ์</th>
                                     <th class="py-3 px-6 rounded-tr-2xl text-right">การดำเนินการ</th>
