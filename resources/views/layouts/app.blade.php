4a55f61a73e92e35bfa8fb332cb22792d0b11fb2
Atthapol <69508240+bankhk10@users.noreply.github.com>
2025-07-22 15:06:41 +0700

    de

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
             x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0"
             x-transition:leave-end="-translate-x-full"
             class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto bg-blue-800 lg:hidden" x-cloak>
-            @include('layouts.mobile-menu')
+            {{-- @include('layouts.mobile-menu') --}}
         </div>
 
     </div>
