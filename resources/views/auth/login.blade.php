957b06aa721564ad8474ac786fd36d1dc5ba2fdd
ATTHAPOL\Atthapol <Atthapol>
2026-07-08 16:17:13 +0700

    add

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ระบบทะเบียนออนไลน์ (RGO)</title>
+    <title>ทะเบียนออนไลน์ (RGO)</title>
     <link rel="icon" type="image/png" href="{{ asset('images/logo2.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@400;700&display=swap" rel="stylesheet">
@@ -132,7 +132,7 @@
     </style>
 </head>
 
-<body class="relative bg-gray-100 min-h-screen flex flex-col items-center justify-center overflow-hidden">
+<body class="relative bg-gray-100 min-h-screen flex flex-col items-center justify-center ">
 
     <div class="absolute top-0 left-0 h-full z-0">
         <svg viewBox="100 280 600 800" xmlns="http://www.w3.org/2000/svg" class="w-full h-full">
@@ -160,13 +160,9 @@
 
     <div class="relative z-20 flex flex-col items-center p-4">
         <img alt="Logo" class="mb-10 h-48 w-48 md:h-56 md:w-56" src="{{ asset('images/logo2.png') }}" />
-
-
-        <h1 class="text-3xl font-bold text-gray-800 mb-8">ระบบทะเบียนออนไลน์</h1>
-
+        <h1 class="text-3xl font-bold text-gray-800 mb-8">ทะเบียนออนไลน์</h1>
         <div class="bg-gray-200 shadow-md rounded-2xl px-8 py-8 w-80 md:w-96">
             <h2 class="text-2xl font-semibold text-gray-700 text-center mb-6">เข้าสู่ระบบ</h2>
-
             <form method="POST" action="{{ route('admin.login') }}" class="space-y-6">
                 @csrf
                 <div class="relative">
@@ -192,7 +188,7 @@
                 <button type="submit"
                     class="w-full bg-green-600 hover:bg-green-700 text-white font-semibold py-3 rounded-xl shadow-md transition duration-300 flex items-center justify-center space-x-2">
                     <span>เข้าสู่ระบบ</span>
-                    <i class="fas fa-arrow-right"></i>
+                    <!-- <i class="fas fa-arrow-right"></i> -->
                 </button>
             </form>
         </div>
